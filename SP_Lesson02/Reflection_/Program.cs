d8dd6f70d190b4771ce46e22ade1b90740f9a610
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-12-03 12:48:40 +0300

    Add files via upload

---
+++ SP_Lesson02/Reflection_/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+//Type type = typeof (string);
+//Console.WriteLine (type);
+
+//MethodInfo[] methodInfos = type.GetMethods ();
+//foreach (MethodInfo method in methodInfos)
+//    Console.WriteLine (method);
+
+//FieldInfo[] fields = type.GetFields (
+//    BindingFlags.Public | BindingFlags.NonPublic
+//    | BindingFlags.Instance | BindingFlags.Static);
+//foreach (FieldInfo field in fields)
+//    if (field.Name == "_firstChar") {
+//        string hello = "Hello";
+//        field.SetValue (hello, 'h');  // записать поле field в объекте "Hello"
+//        char c = (char) field.GetValue (hello);  // прочитать поле field в объекте "Hello"
+//        Console.WriteLine (c);
+//    }
+
+Assembly assembly = Assembly.LoadFrom ("Reflection_.dll");
+Module module = assembly.GetModule ("Reflection_.dll");
+
+foreach (Type type in module.GetTypes ())
+    Console.WriteLine (type);
---
+++ SP_Lesson02/TaskManager_/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskManager_ {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow () {
+            InitializeComponent ();
+
+            Process[] processes = Process.GetProcesses ()
+                .OrderBy (process => process.ProcessName)
+                .ToArray ();
+            DataContext = processes;
+        }
+
+        private void ButtonKill_Click (object sender, RoutedEventArgs e) {
+            // Process selected = (Process) list.SelectedItem;
+            // selected.Kill ();
+        }
+    }
+}
